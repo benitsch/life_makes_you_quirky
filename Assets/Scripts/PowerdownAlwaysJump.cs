71784696b93fa4abeff6c301f635e9a9e810d361
amplejoe <amplejoe@pm.me>
2022-12-10 14:06:14 +0100

    most powerdowns

---
+++ Assets/Scripts/PowerdownAlwaysJump.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerdownAlwaysJump : MonoBehaviour
+{
+    [SerializeField] private int _heartValue = 10;
+    [SerializeField] private string _text = "Always Jump";
+    private bool _pickupButtonPressed; // 'E'
+    private PlayerController _playerController;
+    //[SerializeField] private TextMeshProUGUI _textMesh; // reference
+    private TextMeshProUGUI _textMesh; // reference
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textMesh = gameObject.GetComponentInChildren(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        setText("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _pickupButtonPressed = Input.GetKey(KeyCode.E);
+    }
+
+     // Player Collision CODE
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        if (!isPlayer(other.gameObject.name)) {return;}
+        setText(_text);
+
+    }
+
+    bool isPlayer(string nameString) {
+        return (nameString == "Player");
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //Debug.Log("Hello");
+        if (!isPlayer(other.gameObject.name)) {return;}
+        applyPowerdown();
+    }
+
+    public void applyPowerdown()
+    {
+        if (_pickupButtonPressed)
+        {
+            _playerController.IsAlwaysJumpingActive = !_playerController.IsAlwaysJumpingActive;
+            _playerController.Hearts += _heartValue;
+            Destroy(gameObject);
+        }
+    }
+
+    void setText(string txt)
+    {
+        _textMesh.SetText(txt);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        if (!isPlayer(other.gameObject.name)) {return;}
+        setText("");
+    }
+
+}
---
+++ Assets/Scripts/PowerdownAnnoyingMusic.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerdownAnnoyingMusic : MonoBehaviour
+{
+    [SerializeField] private int _heartValue = 2;
+    [SerializeField] private string _text = "Annoying Music";
+    private bool _pickupButtonPressed; // 'E'
+    private PlayerController _playerController;
+    //[SerializeField] private TextMeshProUGUI _textMesh; // reference
+    private TextMeshProUGUI _textMesh; // reference
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textMesh = gameObject.GetComponentInChildren(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        setText("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _pickupButtonPressed = Input.GetKey(KeyCode.E);
+    }
+
+     // Player Collision CODE
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        if (!isPlayer(other.gameObject.name)) {return;}
+        setText(_text);
+
+    }
+
+    bool isPlayer(string nameString) {
+        return (nameString == "Player");
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //Debug.Log("Hello");
+        if (!isPlayer(other.gameObject.name)) {return;}
+        applyPowerdown();
+    }
+
+    public void applyPowerdown()
+    {
+        if (_pickupButtonPressed)
+        {
+            //TODO: call _playerController.annoyingMusic();
+            _playerController.Hearts += _heartValue;
+            Destroy(gameObject);
+        }
+    }
+
+    void setText(string txt)
+    {
+        _textMesh.SetText(txt);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        if (!isPlayer(other.gameObject.name)) {return;}
+        setText("");
+    }
+
+}
